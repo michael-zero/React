59b9ab0002a37cfb561ffe4ab95d1199a7c1d49f
Michael <mlmbufpi@gmail.com>
2021-09-21 11:40:17 -0300

    formulario funcional - base

--- src/App.js
+++ src/App.js
@@ -1,40 +1,70 @@
 import React from 'react'
-import Input from './Form/Input';
-import useForm from './Hooks/useForm';
+import RadioDesafio from './Form/RadioDesafio';
+
+const perguntas = [
+  {
+    pergunta: "Qual método é utilizado para criar componentes?",
+    options: [
+      'React.makeComponent()',
+      'React.createComponent()',
+      'React.createElement()',
+    ],
+    resposta: "React.createElement()",
+    id: "p1"
+  },
+  {
+    pergunta: "Como importamos um componente externo?",
+    options: [
+      'import Component form "./Component"',
+      'require("./Component")',
+      'import "./Component"',
+    ],
+    resposta: 'import Component form "./Component"',
+    id: "p2"
+  },
+  {
+    pergunta: "Qual hook não é nativo?",
+    options: [
+      'useEffect()',
+      'useFetch()',
+      'useCallback()',
+    ],
+    resposta: 'useFetch()',
+    id: "p3"
+  },
+  {
+    pergunta: "Qual palavra deve ser utilizada para criarmos um hook",
+    options: [
+      'use',
+      'get',
+      'set'
+    ],
+    resposta: 'use',
+    id: "p4"
+  }
+]
 
 function App() {
   
-  const cep = useForm('cep')
-  const email = useForm('email')
-  const nome = useForm()
-  const sobrenome = useForm(false)
-  //  ^ - começa com 5 dígitos
-  //  $ - termina com 3 digitos
-  // -? - poder ter o não o "-"
+  const [respostas, setRespostas] = React.useState({
+    p1: '',
+    p2: '',
+    p3: '',
+    p4: '',
+  })
 
-
-
-  const handleSubmit = (event) => {
-    event.preventDefault()
-    if(cep.validate() && email.validate() && nome.validate()){
-      console.log("Enviou!")
-    }else{
-      console.log("Não deve enviar!")
-    }
+  const handleChange = ({target}) => {
+    setRespostas({...respostas, [target.id]: target.value})
   }
-
+ 
   return ( 
-  <form onSubmit={handleSubmit}>
-      <Input label='Nome' type="text" id="nome" {...nome}/>
-      <Input label='Sobrenome' type="text" id="sobrenome" {...sobrenome}/>
-      <Input label='CEP' type="text" id="cep" placeholder="00000-000" 
-       {...cep}
-      />
-       <Input label='Email' type="email" id="cep" {...email}/>
-      
-      {/* {error && <p>{error}</p>} */}
-
-      <button>Enviar</button>
+  <form>
+    {
+      perguntas.map(pergunta => <RadioDesafio 
+        value={respostas[pergunta.id]}
+        key={pergunta.id} onChange={handleChange} {...pergunta}/>)
+    }
+    <button>Enviar</button>
   </form>
 );
 }
---
+++ src/Form/RadioDesafio.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const RadioDesafio = ({pergunta, id, options, onChange, value}) => { 
+    return (
+        <fieldset style={{padding: '2rem', marginBottom: "1rem", border: "2px solid #eee"}}>
+            <legend style={{fontWeight: "bold"}}>{pergunta}</legend>
+            {
+                // Vetor de respostas 
+                options.map(option => <label key={option} style={{marginBottom: "1rem", fontFamily: 'monospace'}}>
+                    {/* Criando o radio da resposta */}
+                    <input 
+                    id={id}
+                    checked={value === option}
+                    onChange={onChange} value={option} type='radio'/>
+                {option}
+                </label>)
+            }
+        </fieldset>
+    )
+}
+
+export default RadioDesafio
