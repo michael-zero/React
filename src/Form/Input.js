ff1e4279f8bbede3777d3a5b51175aba2f715f9c
Michael <mlmbufpi@gmail.com>
2021-09-21 11:02:01 -0300

    useForm

--- src/Form/Input.js
+++ src/Form/Input.js
@@ -1,12 +1,15 @@
 import React from 'react'
 
-const Input = ({label, id, value, onChange, ...outrasProps}) => {
+const Input = ({label, id, value, onChange, type, onBlur, placeholder, error}) => {
     return (
         <>
             <label htmlFor={id}>{label}</label>
             <input 
-            {...outrasProps}
-            id={id} type="text" value={value} onChange={onChange} />
+            type={type}
+            onBlur={onBlur}
+            placeholder={placeholder}
+            id={id} value={value} onChange={onChange} />
+            {error && <p>{error}</p>}
         </>
     )
 }
